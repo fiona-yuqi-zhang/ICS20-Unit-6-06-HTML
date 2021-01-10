ea39705d5a9576a0d838f3de4ac5d7613ba46470
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2021-01-10 22:17:09 +0000

    script

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let age = 14
-document.getElementById("submit").addEventListener('click', number)
-function number() {
-  age = document.getElementById('age').value
+document.getElementById('submit').addEventListener('click', number)
+  function number() {
+    ge = document.getElementById('age').value
 if (age > 14) {
-  alert('Too big! try again!')
-}
-else if (age < 14) {
-  alert ('Too small! try again!')
-}
+  alert('Too big! try again!')}
+  else if (age < 14) {
+    alert ('Too small! try again!')}
 else {
   alert('You guess it right!')
-}
-}
+  }
+    }
