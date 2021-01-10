368d3d0d4e74d1c365d5badcfc027e8ff835846a
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2021-01-10 22:07:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let age = 14
+document.getElementById("submit").addEventListener('click', number)
+function number() {
+  age = document.getElementById('age').value
+if (age > 14) {
+  alert('Too big! try again!')
+}
+else if (age < 14) {
+  alert ('Too small! try again!')
+}
+else {
+  alert('You guess it right!')
+}
+}
